9f6e7b2d84b2629a59cdfc4399416dd20f033c33
Emil <31871449+EmilVega@users.noreply.github.com>
2018-04-07 23:01:19 -0500

    Creacion de los models.py
     de delivery_product, order y calification

--- calification/models.py
+++ calification/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Calification (models.Model):
+    id_delivery= models.IntegerField()
+    score= models.IntegerField()
+    comment= models.IntegerField()
+
+    def __string__(self):
+        return (self.id_delivery
+                + "\nScore: " + self.score
+                +"\nComment: " + self.comment
+                )
